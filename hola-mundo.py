633bbd48ef1a49666af5af6f5f31c51fb12fa7cf
Wifth <alexander.obando.os2206@gmail.com>
2026-03-30 19:07:43 -0500

    Avance 1

--- hola-mundo.py
+++ hola-mundo.py
@@ -1,1 +1,13 @@
-print("Hola Mundo desde python");
+edad : int = 18
+altura : float = 1.8
+nom : str = "Carlos"
+ape = 'chan'
+esVerde : bool = False
+
+print (f"{nom} {ape} tiene {edad} años y su carro {"es" if esVerde else "no es"} verde")
+
+#operador ternario
+"""""
+es = edad > 18 ? "Mayor edad" : "niño";
+es = "Mayor edad" if edad > 18 else "niño"
+"""""
